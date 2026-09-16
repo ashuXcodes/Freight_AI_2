0071ffe3504be40006e1e59eb9d71c4e7443a812
Ashutosh Kumar <ashutoshkukumar@gmail.com>
2026-09-16 21:20:15 +0530

    fixed seed catalogue

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,7 @@
+import dns from 'node:dns';
 import mongoose from 'mongoose';
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 export async function connectDatabase(mongoUri) {
   await mongoose.connect(mongoUri);
